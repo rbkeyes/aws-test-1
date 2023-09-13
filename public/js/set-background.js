151c5b72b2f33bcb14bc7ca7ee150d47add586b3
Reed Keyes <rbkeyes@gmail.com>
2023-09-13 10:51:41 -0700

    installed dependencies

--- public/js/set-background.js
+++ public/js/set-background.js
@@ -1,3 +1,6 @@
+// set var idx to hour of day
 var idx = Math.floor(new Date().getHours());
+// get body element from document
 var body = document.getElementsByTagName("body")[0];
+// set className of body based on hour of day 
 body.className = "heaven-" + idx;
